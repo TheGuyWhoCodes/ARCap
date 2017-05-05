d39676e8fe58a51652e044a709e752add59c070b
theguywhocodes <christopherariagno@gmail.com>
2017-05-05 16:42:13 -0500

    Just finished up adding the webcam-capture library

--- src/MainThread.java
+++ src/MainThread.java
@@ -1,5 +1,28 @@
+import javax.swing.JFrame;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+
 /**
  * Created by chris on 5/1/2017.
  */
 public class MainThread {
+	static Webcam webcam = Webcam.getDefault();
+    public static void main(String[] args) {
+    	webcam.open();
+    	webcam.setViewSize(WebcamResolution.VGA.getSize());
+		WebcamPanel panel = new WebcamPanel(webcam);
+		panel.setFPSDisplayed(true);
+		panel.setDisplayDebugInfo(true);
+		panel.setImageSizeDisplayed(true);
+		panel.setMirrored(true);
+
+		JFrame window = new JFrame("Test webcam panel");
+		window.add(panel);
+		window.setResizable(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setVisible(true);
+    }
 }
