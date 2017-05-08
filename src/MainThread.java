f5787db6b4b331bd23825e9561e4fb808a58448f
Chris Ariagno <christopherariagno@gmail.com>
2017-05-07 19:43:18 -0500

    Added some structure + neccecary libs

--- src/MainThread.java
+++ src/MainThread.java
@@ -8,17 +8,25 @@ import com.github.sarxos.webcam.WebcamResolution;
  * Created by chris on 5/1/2017.
  */
 public class MainThread {
-	static Webcam webcam = Webcam.getDefault();
+	public static Webcam webcam = Webcam.getDefault();
+	public static WebcamPanel panel;
     public static void main(String[] args) {
-    	webcam.open();
-    	webcam.setViewSize(WebcamResolution.VGA.getSize());
-		WebcamPanel panel = new WebcamPanel(webcam);
+    	resetWebcam();
+		if(webcam.isOpen()){
+			loadWindow();
+		}
+
+    }
+
+    public static void resetWebcam(){
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		panel  = new WebcamPanel(webcam);
 		panel.setFPSDisplayed(true);
 		panel.setDisplayDebugInfo(true);
 		panel.setImageSizeDisplayed(true);
-		panel.setMirrored(true);
-
-		JFrame window = new JFrame("Test webcam panel");
+    }
+    public static void loadWindow(){
+		JFrame window = new JFrame("ARCap");
 		window.add(panel);
 		window.setResizable(true);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
